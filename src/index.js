78d9070e12372abe48177b9ad9f14af3c0f8c32b
Roman Popov <djrulik@gmail.com>
2023-08-18 01:21:19 +0300

    feat: update code

--- src/index.js
+++ src/index.js
@@ -13,25 +13,53 @@ module.exports = function check(str, bracketsConfig) {
       compareSource.push((bracketsConfig[key])[item])
     }
   }
-  //console.log(compareSource, 'compareSource')
+  let csl = compareSource.length
+                                    // console.log(compareSource, 'compareSource', compareSource.length, typeof(compareSource.length))
   str = str.split('')
   //console.log('length ' + str.length);
+  let counter = 0;
+
+    for (let i = 0; i < str.length; i++){
+      for (let j = 0; j < compareSource.length; j = j + 2){
+        if (str[i] == compareSource[j]){
+          counter++
+        } else if (str[i] == compareSource[j+1]) {
+          counter--
+        } 
+
+      }
+      if (counter < 0) {
+        return false;
+      }
+    }
+    
+
+  return true;
+
+
+}
+/* внизу код частично рабочий до пометки конец кода
   while (str.length > 0) {
     for (let i = 0; i < str.length; i++){
       //console.log(str.length, 'let start')
       for (let j = 0; j < compareSource.length; j = j + 2){
-        if ((str[i] == compareSource[j+1] && str[i+1] == compareSource[j])){
+        if ( (csl > 2) && (str[i] == compareSource[j+1] && str[i+1] == compareSource[j])){
           return false;
         }
-        if ((str[i] == compareSource[j] && str[i+2] == compareSource[j+1])){
+        if ((str[0] == compareSource[j] && str[i+2] == compareSource[j+1])){
           return false;
         }
+        if ( (csl > 2) &&(str[i] == compareSource[j] && str[i+2] == compareSource[j+1])){
+          return false;
+        }
+        
         if (str[i] == compareSource[j] && str[i+1] == compareSource[j+1]){
           if (str.length == 2){
             return true;
           }
           str.splice(i, 2);
         }
+        
       }
     }
     
@@ -40,3 +68,4 @@ module.exports = function check(str, bracketsConfig) {
   //return true;
   console.log(str.length, str, 'str after while')
 }
+конец кода */
